1fcf94974e3b10b83198f3bde37cb90a000e4132
qalli <53999110+qalli@users.noreply.github.com>
2020-08-03 00:02:20 +0300

    ChatEklentisi - 03-08-2020-00-12

---
+++ Assets/Scripts/Controller/Gamemanager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    [SerializeField]
+    List<Message> messageList = new List<Message>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            SendMessagetochat("you pressed the space");
+
+    }
+
+    public void SendMessagetochat(string text)
+    {
+        Message newMessage = new Message();
+        newMessage.text = text;
+        messageList.Add(newMessage);
+          
+    }
+
+    [System.Serializable]
+
+    public class Message
+    {
+        public string text;
+    }
+
+}
